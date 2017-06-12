08bab7a25d28a4c688784a16cbab5e1b69d48d69
XL <xlzhong123@outlook.com>
2017-06-12 17:32:16 +0800

    Create ClassTreeTools.py

--- PythonLearnOne/ClassTreeTools.py
+++ PythonLearnOne/ClassTreeTools.py
@@ -1,7 +1,10 @@
+
 # -*- coding: utf-8 -*-
 # @Time   : 2017/5/30
 # @Author : XL ZHONG
 # @File   : ClassTreeTools.py
+from sklearn.tree import DecisionTreeClassifier, _tree
+import numpy as np
 
 class TreeTools:
     '''
@@ -12,8 +15,7 @@ class TreeTools:
 
     def tree_split_thres(self,x,y):
 
-        from sklearn.tree import DecisionTreeClassifier, _tree
-        import numpy as np
+
         from sklearn import tree
 
         clf = DecisionTreeClassifier(criterion="entropy", max_depth=10, max_leaf_nodes=10)
@@ -42,22 +44,67 @@ class TreeTools:
         return new_threshold_2
 
 
-    def tree_discrete(self,x,threshold,quantitle=False):
+    def tree_discrete(self,x,threshold):
         '''
-        这里调用了 Pandas 的cut 函数，如果是使用上面给出的分 BIN 的 阈值则直接调用的是 cut 函数
-        :param x: 
-        :param threshold: 
-        :param quantitle: 如果使用的是分位数的数组来进行离散化
-        :return: 
+        这里调用了 numpy 的 内置函数 searchsorted来实现。
+
+        :param x:
+        :param threshold:
+        :return:
         '''
 
-        import pandas as pd
-        if quantitle==False:
-            x_new=pd.cut(x,threshold)
-        else:
-            x_new=pd.qcut(x,threshold)
-
+        thres_index = np.asarray(threshold).searchsorted(x, side='right')  # 注意这里的 right 表示的是<=
+        thres_index = thres_index.ravel()
+        x_new = []
+        for i in range(len(x)):
+            if thres_index[i] + 1 <= len(threshold):
+                x_new.append(threshold[thres_index[i]])
+            else:
+                x_new.append(x.max())
         return x_new
+
+    def tree_split_discrete(self,x,y):
+
+        clf = DecisionTreeClassifier(criterion="entropy", max_depth=10, max_leaf_nodes=10)
+        clf.fit(x,y)
+        count_leaf=0
+        for i in clf.tree_.children_left:
+            if i==_tree.TREE_LEAF:
+                count_leaf+=1
+        count_leaf
+
+
+        threshold = clf.tree_.threshold  #所有的节点全部是<=
+        # threshold=np.sort(threshold)[count_leaf:]
+        # #这种方式不太好，如果有小于-2的排序就会出问题。
+        #  -2的数目和叶子数相同 后面需要排除掉-2的值
+        count=0
+        for i in threshold:
+            if i==-2: count+=1
+
+        new_threshold=list(filter(lambda x: x!=-2,threshold))
+
+        if count>count_leaf:new_threshold+=[-2]
+
+        new_threshold_2=np.sort(new_threshold)
+
+        thres_index = np.asarray(new_threshold_2).searchsorted(x, side='right')  # 注意这里的 right 表示的是<=
+        thres_index = thres_index.ravel()
+        x_new = []
+        for i in range(len(x)):
+            if thres_index[i] + 1 <= len(new_threshold_2):
+                x_new.append(new_threshold_2[thres_index[i]])
+            else:
+                x_new.append(x.max())#这里后面可以修改
+        return x_new # 返回的数值是右侧的等于的值
+
+
+
+
+
+
+
+
 
 
 
